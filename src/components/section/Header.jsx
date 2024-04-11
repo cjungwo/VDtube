5d5911a8361ca2ffd08add0574737738f2205ef5
Cjungwo <jwoo516@naver.com>
2024-04-11 11:29:59 +1000

    feat sidebar & fix video bugs

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -4,10 +4,16 @@ import Logo from '../header/Logo';
 import Menu from '../header/Menu';
 import Sns from '../header/Sns';
 
-const Header = () => {    
+const Header = () => { 
+    const [ isMenuAction, setIsMenuAction ] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuAction(!isMenuAction);
+    }
+    
     return (
-        <header id='header' role='banner' >
-            <Logo />
+        <header id='header' role='banner' className={isMenuAction ? 'active' : ''} >
+            <Logo toggleMenu={toggleMenu}/>
             <Menu />
             <Sns />
         </header>
