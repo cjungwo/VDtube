baab42bc5c69a57fd03708ba7b18c4969b487b5c
Cjungwo <jwoo516@naver.com>
2024-04-11 10:33:00 +1000

    connect rapid api

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,33 +4,58 @@ import { useParams } from 'react-router-dom';
 import Main from '../components/section/Main';
 
 import VideoSearch from '../components/videos/VideoSearch';
+import { fetchFromAPI } from '../utils/api';
 
-const REACT_APP_YOUTUBE_API_KEY = "AIzaSyCoiev0ghF2PpdXKPePUoOOx7oAT_oCQec";
+// const REACT_APP_YOUTUBE_API_KEY = "AIzaSyCoiev0ghF2PpdXKPePUoOOx7oAT_oCQec";
 
 const Search = () => {
     const { searchId } = useParams();
     const [ videos, setVideos ] = useState([]);
+    const [ nextPageToken, setNextPageToken ] = useState(null);
+    const [ loading, setLoading ] = useState(true);
+
     
     useEffect(() => {
-        fetch(
-            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=48&q=${searchId}&type=video&key=${REACT_APP_YOUTUBE_API_KEY}`,
-        )
-        .then(response => response.json())
-        .then(result => {
-            console.log(result);
-            setVideos(result.items);
+        setVideos([]);
+        fetchVideos(searchId);
+        setLoading(true);
+    }, [searchId]);
+
+    const fetchVideos = (query, pageToken = '') => {
+        fetchFromAPI(`search?part=snippet&q=${query}&pageToken=${pageToken}`)
+        .then((data) => {
+            setNextPageToken(data.nextPageToken);
+            setVideos((prevVideos) => [...prevVideos, ...data.items]);
+            setLoading(false);
         })
-        .catch(error => console.log(error));
-        }, [searchId]);
+        .catch((err) => {
+            console.log('ERROR: ', err);
+            setLoading(false);
+        });
+    }
+
+    const handleLoadMore = () => {
+        if (nextPageToken) {
+            fetchVideos(searchId, nextPageToken);
+        }
+    };
+
+    const searchPageClass = loading ? 'isLoading' : 'isLoaded';
 
     return (
         <Main 
             title = "Search Video"
             description="This is search page.">
             
-            <section id='searchPage'>
+            <section id='searchPage' className={searchPageClass}>
+                <h2>Result: <em>{searchId}</em></h2>
                 <div className="video__inner search">
                     <VideoSearch videos={videos} />
+                </div>
+                <div className="video__more">
+                    {nextPageToken && (
+                        <button onClick={handleLoadMore}>More</button>
+                    )}
                 </div>
             </section>
         </Main>
---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+    params: {
+        maxResults: 48,
+    },
+    headers: {
+        'X-RapidAPI-Key': '0b1b5755b1msh0110cfbe1a29d0ap108175jsn413cff7295b8',
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    },
+};
+
+export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    console.log(data)
+    return data;
+};
